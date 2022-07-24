7a4b4cb9b3c9b19c5da772181bb50c71310d1c67
Keks <keks@htmlacademy.ru>
2022-07-24 19:45:23 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/edit-picture.js
@@ -0,0 +1,151 @@
+const imageElement = document.querySelector('.img-upload__preview img');
+const sliderContainer = document.querySelector('.img-upload__effect-level');
+const sliderElement = document.querySelector('.effect-level__slider');
+const scaleInputElement = document.querySelector('.scale__control--value');
+const effectLevelInputElement = document.querySelector('.effect-level__value');
+const effectsList = document.querySelector('.effects__list');
+
+const MIN_SCALE_IMAGE = 0.25;
+const MAX_SCALE_IMAGE = 1;
+const SCALE_IMAGE_STEP = 0.25;
+const EFFECTS = {
+  chrome: {
+    name: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    units: '',
+    decimal: 1
+  },
+  sepia: {
+    name: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    units: '',
+    decimal: 1
+  },
+  marvin: {
+    name: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    units: '%',
+    decimal: 0
+  },
+  phobos: {
+    name: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    units: 'px',
+    decimal: 1
+  },
+  heat: {
+    name: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    units: '',
+    decimal: 1
+  }
+};
+
+let scale = '';
+
+const zoomIn = () => {
+  let scaleValue = Number(scaleInputElement.value.replace('%', '')) / 100;
+  if (scaleValue > MIN_SCALE_IMAGE) {
+    scaleValue -= SCALE_IMAGE_STEP;
+  }
+  scaleInputElement.value = `${(scaleValue * 100).toFixed(0)}%`;
+  scale = `scale(${scaleValue})`;
+  imageElement.style.transform = scale;
+};
+
+const zoomOut = () => {
+  let scaleValue = Number(scaleInputElement.value.replace('%', '')) / 100;
+  if (scaleValue < MAX_SCALE_IMAGE) {
+    scaleValue += SCALE_IMAGE_STEP;
+  }
+  scaleInputElement.value = `${(scaleValue * 100).toFixed(0)}%`;
+  scale = `scale(${scaleValue})`;
+  imageElement.style.transform = scale;
+};
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+});
+
+const applyEffect = (event) => {
+  imageElement.removeAttribute('class');
+  imageElement.removeAttribute('style');
+  if (scale) {
+    imageElement.setAttribute('style', `transform: ${scale};`);
+  }
+  if (!event.target.matches('#effect-none')) {
+    sliderContainer.classList.remove('hidden');
+    const effectClass = `effects__preview--${event.target.value}`;
+    imageElement.classList.add(effectClass);
+    const activeEffect = effectsList.querySelector('[type="radio"]:checked').value;
+    const effect = EFFECTS[activeEffect];
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: effect.min,
+        max: effect.max,
+      },
+      start: effect.max,
+      step: effect.step,
+    });
+    effectLevelInputElement.value = effect.max;
+  } else {
+    imageElement.removeAttribute('style');
+    sliderContainer.classList.add('hidden');
+    effectLevelInputElement.value = '';
+    scaleInputElement.value = '100%';
+    scale = '';
+  }
+};
+
+const changeEffect = () => {
+  const sliderValue = sliderElement.noUiSlider.get();
+  const activeEffect = effectsList.querySelector('[type="radio"]:checked').value;
+  if (activeEffect !== 'none') {
+    const effect = EFFECTS[activeEffect];
+    const filterValue = Number(sliderValue).toFixed(effect.decimal);
+    let styleValue = '';
+    if (scale) {
+      styleValue = `transform: ${scale}; filter: ${effect.name}(${filterValue}${effect.units})`;
+    } else {
+      styleValue = `filter: ${effect.name}(${filterValue}${effect.units})`;
+    }
+    imageElement.setAttribute('style', styleValue);
+    effectLevelInputElement.value = filterValue;
+  } else {
+    effectLevelInputElement.value = '';
+  }
+};
+
+const resetEffect = () => {
+  imageElement.removeAttribute('class');
+  imageElement.removeAttribute('style');
+  sliderContainer.classList.add('hidden');
+  effectLevelInputElement.value = '';
+  scaleInputElement.value = '100%';
+  scale = '';
+};
+
+sliderElement.noUiSlider.on('update', changeEffect);
+
+export {
+  zoomIn,
+  zoomOut,
+  applyEffect,
+  resetEffect
+};
---
+++ 11/js/upload-form-sender.js
@@ -0,0 +1,86 @@
+import {
+  sendData
+} from './api.js';
+import {
+  showError
+} from './functions.js';
+import {
+  closeUploadForm
+} from './upload-form-toggle.js';
+import {
+  showResultMessage
+} from './upload-form-result.js';
+
+const uploadForm = document.querySelector('.img-upload__form');
+const uploadFormHashtag = uploadForm.querySelector('.text__hashtags');
+const uploadFormButton = uploadForm.querySelector('.img-upload__submit');
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'error-text'
+});
+
+const validationHashtag = (value) => {
+  let isValid = true;
+  if (value.length) {
+    const hashtags = value.toLowerCase().split(' ');
+    if (hashtags.length > 5) {
+      isValid = false;
+    }
+    const uniqueHashtags = [];
+    hashtags.forEach((hashtag) => {
+      const correctValue = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+      if (!correctValue.test(hashtag)) {
+        isValid = false;
+      }
+      if (uniqueHashtags.includes(hashtag)) {
+        isValid = false;
+      }
+      uniqueHashtags.push(hashtag);
+    });
+  }
+  return isValid;
+};
+
+pristine.addValidator(
+  uploadFormHashtag,
+  validationHashtag,
+  'Некорректный формат. Хэштег должен начинаться с символа #, не содержать пробелы и спецсимволы, длина хэштега должна быть от 2 до 20 символов. Не более 5 хэштегов, разделенных пробелом.'
+);
+
+const disableFormButton = () => {
+  uploadFormButton.disabled = false;
+  uploadFormButton.textContent = 'Идет публикация...';
+};
+
+const enableFormButton = () => {
+  uploadFormButton.disabled = false;
+  uploadFormButton.textContent = 'Опубликовать';
+};
+
+const onUploadFormSubmit = (event) => {
+  event.preventDefault();
+  if (pristine.validate()) {
+    disableFormButton();
+    sendData(
+      () => {
+        showResultMessage('success');
+        enableFormButton();
+        closeUploadForm();
+        uploadForm.reset();
+      },
+      () => {
+        showResultMessage('error');
+        enableFormButton();
+      },
+      new FormData(event.target),
+    );
+  } else {
+    showError('Форма заполнена некорректно!');
+  }
+};
+
+export {
+  onUploadFormSubmit
+};
